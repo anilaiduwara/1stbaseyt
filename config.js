8d86ea64e8bf285ee54ec46271c3c3961d530d3c
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-11-16 11:35:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG ||"",
+ALIVE_MSG: process.env.ALIVE_MSG ||"hello i am anila lochana",
 };
