93bc8193dcd4ebe55d4fc19f6d765952cab81f54
cc1sell <githubccsell@gmail.com>
2024-11-16 18:18:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-uconst fs = require('fs');
+const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
