df91ec675a426101570e7a2754ce35d999c3da10
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-11-16 18:27:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "buoEDAQT#mE3RsU7gU34SwSGw5i8RMFB75__AQgVJryUkMHijBP0",
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/3f7249eb429c8211cbba3.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"hello i am anila lochana",
 };
