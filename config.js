074733de046eb2a140b0e547736f1dc2afc9a6d8
cc1sell <githubccsell@gmail.com>
2024-11-16 18:16:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+uconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "buoEDAQT#mE3RsU7gU34SwSGw5i8RMFB75__AQgVJryUkMHijBP0",
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://telegra.ph/file/3f7249eb429c8211cbba3.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"hello i am anila lochana",
 };
