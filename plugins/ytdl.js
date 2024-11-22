02c7314aae5b1b713ab558c746e278ed92b5a022
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-11-22 19:26:54 +0530

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -2,7 +2,6 @@
 const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
 
-const apilink = 'https://www.dark-yasiya-api.site' // API LINK ( DO NOT CHANGE THIS!! )
 
 cmd({
     pattern: "song",
@@ -16,11 +15,11 @@ try{
 
 if(!q) return reply('Give me song name or url !')
     
-const search = await fetchJson(`${apilink}/search/yt?q=${q}`)
+const search = await fetchJson(`https://www.dark-yasiya-api.site/search/yt?text=${q}`)
 const data = search.result.data[0];
 const url = data.url
     
-const ytdl = await fetchJson(`${apilink}/download/ytmp3?url=${data.url}`)
+const ytdl = await fetchJson(`https://www.dark-yasiya-api.site/download/ytmp3?url=${data.url}`)
     
 let message = `‎‎           🎶 YT SONG DOWNLOADER 🎶
 
