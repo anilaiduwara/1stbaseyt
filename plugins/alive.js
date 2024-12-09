0906b38e0ac8ed0329676b4af2fe0730d133fa6d
Owner by locha <156207819+anilaiduwara@users.noreply.github.com>
2024-12-09 08:51:59 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -14,6 +14,23 @@ return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: conf
 console.log(e)
 reply(`${e}`)
 }
+});
+cmd({
+    pattern: "restart",
+    desc: "restart the bot",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+const {exec} = require("child_process")
+reply("restarting...")
+await sleep(1500)
+exec("pm2 restart all")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
 })
 
 
